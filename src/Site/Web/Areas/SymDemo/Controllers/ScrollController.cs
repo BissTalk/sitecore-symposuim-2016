73f4c5b9b073c7591aa45e05d8c691069bd66ebb
Bissol, Joseph <joseph_bissol@cable.comcast.com>
2016-09-06 15:01:30 -0400

    Add scroll controller

---
+++ src/Site/Web/Areas/SymDemo/Controllers/ScrollController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SymDemo.Site.Web.Areas.SymDemo.Controllers
+{
+    public class ScrollController : Controller
+    {
+        // GET: SymDemo/Scroll
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
