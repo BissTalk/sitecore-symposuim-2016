52d6652087981c9e2863a0fe57e5586d523a1c2f
Bissol, Joseph <joseph_bissol@cable.comcast.com>
2016-09-06 19:56:00 -0400

    Added page editing ability and animation/ccs transitions

--- src/Site/Web/Areas/SymDemo/Controllers/ScrollController.cs
+++ src/Site/Web/Areas/SymDemo/Controllers/ScrollController.cs
@@ -1,17 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Web.Mvc;
+using Sitecore;
+using SymDemo.Site.Models.Items;
 
 namespace SymDemo.Site.Web.Areas.SymDemo.Controllers
 {
-    public class ScrollController : Controller
+    /// <summary>
+    ///     Controller for the sroll feature(s).
+    /// </summary>
+    /// <seealso cref="System.Web.Mvc.Controller" />
+    public class ScrollController : GlassController
     {
-        // GET: SymDemo/Scroll
-        public ActionResult Index()
+        private readonly HttpContextBase _httpContext;
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ScrollController" /> class.
+        /// </summary>
+        /// <param name="httpContext">The HTTP context.</param>
+        /// <param name="sitecoreContext">The Sitecore context.</param>
+        public ScrollController([NotNull] HttpContextBase httpContext, [NotNull] ISitecoreContext sitecoreContext):
+            base(sitecoreContext)
         {
-            return View();
+            if(httpContext == null) throw new ArgumentNullException(nameof(httpContext));
+            if(sitecoreContext == null) throw new ArgumentNullException(nameof(sitecoreContext));
+
+            _httpContext = httpContext;
+        }
+
+        /// <summary>
+        ///     The default controller action.
+        /// </summary>
+        /// <returns>An MVC view</returns>
+        public override ActionResult Index()
+        {
+            var d = DataSourceItem;
+            var model = GetLayoutItem<ScrollSource>();
+            return View(model);
         }
     }
 }
