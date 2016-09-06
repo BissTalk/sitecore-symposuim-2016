b03bc1ab2d7326a7f8fcedf0a4445430ef06f58e
Bissol, Joseph <joseph_bissol@cable.comcast.com>
2016-09-06 11:03:33 -0400

    Add Sitecore Model for Page

---
+++ src/Common/Common.Models/Items/Page.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace SymDemo.Common.Models.Items
+{
+    /// <summary>
+    ///     An item representing a Page.
+    /// </summary>
+    /// <seealso cref="SymDemo.Common.Models.GlassBase" />
+    /// <seealso cref="SymDemo.Common.Models.Items.IPage" />
+    public partial class Page
+    {
+        [SitecoreField("__semantics")]
+        public virtual IEnumerable<Guid> Tags { get; set; }
+    }
+}
